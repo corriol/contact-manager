a7166baa02820f2e5281cd10b5b218c496ed4b0a
corriol <vjorda@gmail.com>
2021-04-07 09:58:44 +0200

    Part 2. Pas 1

--- contact-create.php
+++ contact-create.php
@@ -46,180 +46,122 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link href="/assets/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/assets/css/global.css" rel="stylesheet">
-    <title>Contact manager</title>
-    <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/jquery.min.js"></script>
-    <script src="/assets/js/popper.min.js"></script>
+<?php include 'partials/header.partial.php' ?>
 
-</head>
-<body>
-
-<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="index.php">Contact Manager</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
-            data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href=""></a>
-        </li>
-    </ul>
-</nav>
-
-<div class="container-fluid">
-    <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div class="sidebar-sticky pt-3">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">
-                            Contacts <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">
-                            Option 2
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">
-                            Option 2
-                        </a>
-                    </li>
-
+    <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">New contact</h1>
+            <!--<div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group mr-2">
+                    <a href="#" class="btn btn-sm btn-outline-secondary">New contact</a>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Other option</button>
+                </div>
+            </div>-->
+        </div>
+        <?php if ($isPost && !empty($errors)): ?>
+            <div class="container">
+                <ul class="text-danger">
+                    <?php foreach ($errors as $error) : ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
                 </ul>
-
             </div>
-        </nav>
-
-        <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">New contact</h1>
-                <!--<div class="btn-toolbar mb-2 mb-md-0">
-                    <div class="btn-group mr-2">
-                        <a href="#" class="btn btn-sm btn-outline-secondary">New contact</a>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Other option</button>
+        <?php endif; ?>
+        <form class="row" action="contact-create.php" method="post" novalidate>
+            <div class="col-6 form-group">
+                <label for="firstname">First name:</label>
+                <input id="firstname" type="text" name="firstname" value="<?= $firstname ?? "" ?>"
+                       class="form-control" placeholder="First name..." required>
+                <?php if (!empty($errors["firstname"])) : ?>
+                    <div class="invalid-feedback d-block">
+                        <?= $errors["firstname"] ?>
                     </div>
-                </div>-->
+                <?php endif; ?>
             </div>
-            <?php if ($isPost && !empty($errors)): ?>
-                <div class="container">
-                    <ul class="text-danger">
-                        <?php foreach ($errors as $error) : ?>
-                            <li><?= $error ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            <?php endif; ?>
-            <form class="row" action="contact-create.php" method="post" novalidate>
-                <div class="col-6 form-group">
-                    <label for="firstname">First name:</label>
-                    <input id="firstname" type="text" name="firstname" value="<?= $firstname ?? "" ?>"
-                           class="form-control" placeholder="First name..." required>
-                    <?php if (!empty($errors["firstname"])) : ?>
-                        <div class="invalid-feedback d-block">
-                            <?= $errors["firstname"] ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="form-group col-6">
-                    <label for="lastname">Last name:</label>
-                    <input id="lastname" type="text" name="lastname" value="<?= $lastname ?? "" ?>"
-                           class="form-control" placeholder="Last name..." required>
-                </div>
+            <div class="form-group col-6">
+                <label for="lastname">Last name:</label>
+                <input id="lastname" type="text" name="lastname" value="<?= $lastname ?? "" ?>"
+                       class="form-control" placeholder="Last name..." required>
+            </div>
 
-                <div class="form-group col-6">
-                    <label for="phone">Phone:</label>
-                    <input id="phone" type="text" name="phone" value="<?= $phone ?? "" ?>"
-                           class="form-control" placeholder="Phone number..." required>
-                </div>
+            <div class="form-group col-6">
+                <label for="phone">Phone:</label>
+                <input id="phone" type="text" name="phone" value="<?= $phone ?? "" ?>"
+                       class="form-control" placeholder="Phone number..." required>
+            </div>
 
-                <div class="form-group col-6">
-                    <label for="email">Email:</label>
-                    <input id="email" type="email" name="email" value="<?= $email; ?>"
-                           class="form-control" placeholder="Email..." required>
-                </div>
+            <div class="form-group col-6">
+                <label for="email">Email:</label>
+                <input id="email" type="email" name="email" value="<?= $email; ?>"
+                       class="form-control" placeholder="Email..." required>
+            </div>
 
-                <div class="form-group col-6">
-                    <label for="address">Address:</label>
-                    <input id="address" type="text" name="address" value="<?= $address ?? "" ?>"
-                           class="form-control" placeholder="Adress...">
-                </div>
+            <div class="form-group col-6">
+                <label for="address">Address:</label>
+                <input id="address" type="text" name="address" value="<?= $address ?? "" ?>"
+                       class="form-control" placeholder="Adress...">
+            </div>
 
-                <div class="form-group col-4">
-                    <label for="city">City:</label>
-                    <input class="form-control" id="city" type="text" name="city"
-                           value="<?= $city ?? "" ?>" placeholder="City...">
-                </div>
+            <div class="form-group col-4">
+                <label for="city">City:</label>
+                <input class="form-control" id="city" type="text" name="city"
+                       value="<?= $city ?? "" ?>" placeholder="City...">
+            </div>
 
-                <div class="form-group col-2">
-                    <label for="zip">Zip Code:</label>
+            <div class="form-group col-2">
+                <label for="zip">Zip Code:</label>
 
-                    <input id="zip" type="text" name="zip" value="<?= $zipCode ?? "" ?>"
-                           class="form-control" placeholder="Zip code...">
-                </div>
+                <input id="zip" type="text" name="zip" value="<?= $zipCode ?? "" ?>"
+                       class="form-control" placeholder="Zip code...">
+            </div>
 
-                <div>
-                    <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Submit
-                    </button>
-                    <button type="reset" class="btn btn-danger"><i
-                                class="fa fa-trash"></i> Reset
-                    </button>
-                </div>
+            <div>
+                <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Submit
+                </button>
+                <button type="reset" class="btn btn-danger"><i
+                            class="fa fa-trash"></i> Reset
+                </button>
+            </div>
 
-            </form>
+        </form>
 
-            <?php if ($isPost && (empty($errors))) : ?>
-                <div class="container">
-                    <div class="row text-center">
-                        <div class="col-lg-7 col-xl-7 card flex-row mx-auto px-0 mt-5 border">
-                            <table class="table">
-                                <tr>
-                                    <th scope="row">First name</th>
-                                    <td><?= $firstname ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last name</th>
-                                    <td><?= $lastname ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Phone</th>
-                                    <td><?= $phone ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Email</th>
-                                    <td><?= $email ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Address</th>
-                                    <td><?= $address ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">City</th>
-                                    <td><?= $city ?></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Zip code</th>
-                                    <td><?= $zipCode ?></td>
-                                </tr>
-                            </table>
-                        </div>
+        <?php if ($isPost && (empty($errors))) : ?>
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-lg-7 col-xl-7 card flex-row mx-auto px-0 mt-5 border">
+                        <table class="table">
+                            <tr>
+                                <th scope="row">First name</th>
+                                <td><?= $firstname ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Last name</th>
+                                <td><?= $lastname ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Phone</th>
+                                <td><?= $phone ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Email</th>
+                                <td><?= $email ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Address</th>
+                                <td><?= $address ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">City</th>
+                                <td><?= $city ?></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Zip code</th>
+                                <td><?= $zipCode ?></td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
-            <?php endif; ?>
-        </main>
-    </div>
-</div>
-</body>
-</html>
+            </div>
+        <?php endif; ?>
+    </main>
+<?php include 'partials/footer.partial.php' ?>
---
+++ partials/header.partial.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link href="/assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/assets/css/global.css" rel="stylesheet">
+    <title>Contact manager</title>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Contact Manager</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
+            data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" href=""></a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div class="sidebar-sticky pt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="">
+                            Option 1 <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">
+                            Option 2
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">
+                            Option 2
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
